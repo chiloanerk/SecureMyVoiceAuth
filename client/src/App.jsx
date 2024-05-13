a68c1306713cb11762dd580f21f47151f7409533
Relebogile <chiloane.rk@gmail.com>
2024-05-13 16:20:43 +0200

    complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import {Route, Routes} from "react-router-dom";
+import {Login, Signup} from "./pages/index.js";
+import Home from "./pages/Home.jsx";
+
 function App() {
     return (
         <div className="App">
-            <h1>Hello</h1>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
         </div>
     );
 }
