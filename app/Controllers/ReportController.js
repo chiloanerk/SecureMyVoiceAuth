08ec267a54b6ce117ccbe2147d78e1040d269c34
reledev <relebogilechiloane@quitch.com>
2024-12-25 18:42:38 +0200

    Prevent duplicate reports and improve error handling.

--- app/Controllers/ReportController.js
+++ app/Controllers/ReportController.js
@@ -5,29 +5,18 @@ class ReportController {
         try {
             const reportData = req.body;
             const { unique_link } = req.params;
-
-            // Validate report data (e.g., required fields, data types)
-
+            
             const newReport = await ReportService.createReport(reportData, unique_link);
             res.status(201).json(newReport);
         } catch (error) {
             console.error(error);
-            if (error.name === 'ValidationError') {
-                res.status(400).json({ message: 'Validation error: ' + error.message });
-            } else if (error.name === 'CastError') {
-                throw new Error('Invalid data type encountered.');
-            }
-            else {
-                throw new Error('Error creating report' + error.message);
-            }
+            res.status(500).json({message: 'Internal Server Error'});
         }
     }
 
     async getReport(req, res) {
         try {
             const { unique_link } = req.params;
-
-            // Implement authorization check here (e.g., using JWTs)
 
             const { status, data, message } = await ReportService.getReportsByUser(unique_link);
             res.status(status).json({message, data});
--- app/Services/ReportService.js
+++ app/Services/ReportService.js
@@ -11,33 +11,48 @@ class ReportService {
                 throw new Error('User not found for the provided unique link.');
             }
 
+            const existingReport = await Report.findOne({
+                user: user._id,
+                description: reportData.description,
+                category: reportData.category,
+                evidence: reportData.evidence,
+            })
+
+            if (existingReport) {
+                return { status: 409, message: 'Duplicate, report already exists!' };
+            }
+
             // Create the report with the associated user
-            const newReport = await Report.create({ ...reportData, user: user._id });
+            const newReport = await Report.create({ ...reportData, user: user._id }, undefined);
 
             return { status: 201, data: newReport }; // Return status code 201 (Created)
         } catch (error) {
             if (error.message === 'User not found for the provided unique link.') {
                 return { status: 404, message: error.message }; // Return 404 Not Found
             } else {
-                return { status: 500, message: 'Internal Server Error' };
+                return { status: 500, message: 'Internal Server Error', error: error };
             }
         }
     }
 
     async getReportsByUser(uniqueLink) {
         try {
-            // Find the report associated with the unique link
-            const reports = await Report.find({
-                'user.unique_link': uniqueLink
-            }).populate('user', '-password');
+            // Find the user associated with the unique link
+            const user = await User.findOne({ 'unique_link': uniqueLink })
+            if (!user) {
+                return { status: 404, message: 'User not found for the provided unique link.' };
+            }
 
-            if (!report || reports.length === 0) {
-                return { status: 404, message: 'No reports found for this user.' }; // Return 404 Not Found
+            const reports = await Report.find({ user: user._id }).populate('user', '-password');
+            console.log("Reports found: ", reports);
+
+            if (!reports || reports.length === 0) {
+                return { status: 404, message: 'No reports found for this user' };
             }
 
             return { status: 200, data: reports }; // Return 200 OK
         } catch (error) {
-            if (error.message === 'Cast error') {
+            if (error.message === 'CastError') {
                 return { status: 400, message: 'Invalid unique link.' };
             } else {
                 return {status: 500, message: 'Internal Server Error: ' + error.message};
